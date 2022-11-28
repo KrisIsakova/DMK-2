c2996f335cb0175b1850db81ff1c5a1c5140673c
KrisIsakova <118714648+KrisIsakova@users.noreply.github.com>
2022-11-28 18:12:13 +0300

    Add files via upload

---
+++ 1 готовая.pas
@@ -0,0 +1,18 @@
+﻿begin
+var x:=1;
+var n:= ReadInteger('Введите количество чисел в массиве А: ');
+var a: array [1..100] of integer;
+  randomize;
+  A[1] := random(100);
+  print('Массив А: ',A[1], ' ');
+  for var i:=2 to n
+  do 
+  begin
+    A[i] := A[i-1] + random(3);
+    print(A[i], ' ');
+    if A[i] <> A[i-1]
+      then inc(x);
+  end;
+  println;
+  println('Количество различных чисел в массиве А: ',x);
+end.
---
+++ 2 готовая.pas
@@ -0,0 +1,30 @@
+﻿begin
+  var s:=readstring('Введите исходную строку: ');
+  var s1:=readstring('Введите подстроку: ');
+  var x:=length(s);
+  var y:=length(s1);
+  var n:=0;
+  var i:=1;
+  var flag:boolean;
+while i<=x-y+1 do
+begin
+if s[i]=s1[1] then
+begin
+  flag:=true;
+for var j:=1 to y-1 do
+if s[i + j]<>s1[j + 1] then
+begin
+i:= i+j;
+flag := false;
+break
+end;
+if flag then
+begin
+ println('Подстрока является подпоследовательностью исходной строки');
+exit;
+end;
+end;
+i += 1;
+end;
+ println('Подстрока не является подпоследовательностью исходной строки');
+end.
